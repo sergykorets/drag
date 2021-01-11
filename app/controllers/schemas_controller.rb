c627842257131612e9bd2205e7ba3e1e78e9e3d1
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2021-01-11 14:35:00 +0200

    2021

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -3,7 +3,11 @@ class SchemasController < ApplicationController
     @admin = Rails.env.development? || (current_user && current_user.admin)
   end
 
-  def skipass;  end
+  def skipass
+    set_meta_tags title: "Cкіпас | Драгобрат",
+                  description: "Ціни на підйомники драгобрат",
+                  keywords: "Скіпас підйомники драгобрат ціни"
+  end
 
   def chat
     @user = current_user && {name: current_user.name, id: current_user.id}
